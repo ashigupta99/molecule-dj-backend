0384921d882f29f6ea498644b3c15450da8bfc85
Aashi Gupta <aashifamily123@gmail.com>
2025-06-28 20:24:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def serve_static(filename):
     return send_from_directory("frontend", filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
